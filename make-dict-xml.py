49858bb052814f74189bf701e8d3ac815adbf659
Dan <dan@wadoku.de>
2021-12-29 16:00:53 +0100

    extract common structure int get_entry_xml() additional index entry without 中点(・) css for domain

--- make-dict-xml.py
+++ make-dict-xml.py
@@ -6,29 +6,29 @@ import re
 import sys
 
 
-def print_xml_header():
+def print_xml_header() -> None:
     print(
         """<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
         <d:dictionary xmlns="http://www.w3.org/1999/xhtml" xmlns:wd="http://www.wadoku.de/xml/entry"
             xmlns:d="http://www.apple.com/DTDs/DictionaryService-1.0.rng">""")
 
 
-def print_xml_footer():
+def print_xml_footer() -> None:
     print("""</d:dictionary>""")
 
 
-def print_usage():
-    print("Usage: %s  <warodai path> [<output file>]" % sys.argv[0])
+def print_usage() -> None:
+    print("Usage: %s  <warodai path>" % sys.argv[0])
 
 
-def get_index_xml(value, title, yomi=None):
+def get_index_xml(value: str, title: str, yomi=None) -> str:
     if yomi:
         return f"""<d:index d:value="{value}" d:title="{title}" d:yomi="{yomi}"/>"""
     else:
         return f"""<d:index d:value="{value}" d:title="{title}"/>"""
 
 
-def get_lines_xml(lines: list[str]):
+def get_lines_xml(lines: list[str]) -> str:
     is_list = False
     ret = ""
     for line in lines:
@@ -62,52 +62,58 @@ def get_lines_xml(lines: list[str]):
     return ret
 
 
-def get_entry_xml(path):
+def get_entry_xml(title: str, file_id: str, index_xml: str, transcription: str, content: str, domain: str = None) -> str:
+    return f"""<d:entry id="{file_id}" d:title="{title}">
+        {index_xml}
+        <div class="entry">
+        <h1>{title} <small>{transcription}</small></h1>
+        {'''<p class="domain">[''' + domain + "]</p>" if domain else ""}
+        <p>{content}</p>
+        </div>
+        </d:entry>"""          
+
+
+def get_entry_xml_from(path) -> str:
     file_id = pathlib.Path(path).stem
     with io.open(path, mode='r', encoding='utf-8') as f:
         lines = f.readlines()
+        # しょしょ【処々･所々･諸所･処処･所所】(сёсё)〔004-99-20〕
+        # TODO ちょうへん, ちょうへんしょうせつ【長篇･長編, 長篇小説･長編小説】(тё:хэн, тё:хэн-сё:сэцу)〔009-26-70〕
+        # TODO リューチューとう【リューチュー島･琉球島】(Рю:тю:-то:) [геогр.]〔008-71-42〕
         header = re.search('^(.+?)【(.+?)】\((.+?)\)〔(\d{3}-\d{2}-\d{2})〕$', lines[0])
         if header:
             (hiragana, kanji, transcription) = header.groups()[0:-1]
             title = f"{hiragana}【{kanji}】"
-            kanji_index_xml = ""
+            index_xml = get_index_xml(hiragana, title)
             for k in kanji.split('･'):
-                kanji_index_xml += get_index_xml(k, k, hiragana)
-            ret = f"""<d:entry id="{file_id}" d:title="{title}">
-{get_index_xml(hiragana, title)}{kanji_index_xml}
-<div class="entry">
-<h1>{title} <small>{transcription}</small></h1>
-<p>{get_lines_xml(lines[1:])}</p>
-</div>
-</d:entry>"""
-            return ret
+                index_xml += get_index_xml(k, k, hiragana)
+            return get_entry_xml(title, file_id, index_xml, transcription, get_lines_xml(lines[1:]))
         else:
             # カルカッタ(Карукатта) [геогр.]〔000-28-00〕
+            # TODO ケソン, ケソン・シティー(Кэсон, Кэсон-Сити:) [геогр.]〔005-06-52〕
             header = re.search('^(.+?)\((.+?)\) \[(.+?)]〔(\d{3}-\d{2}-\d{2})〕$', lines[0])
             if header:
                 (katakana, transcription, domain) = header.groups()[0:-1]
                 title = f"{katakana}"
-                hiragana = katakana  # TODO add hiragana to index
-                ret = f"""<d:entry id="{file_id}" d:title="{title}">
-{get_index_xml(hiragana, title)}
-<h1>{title} <small>{transcription}</small></h1>
-<p>[{domain}]</p>
-<p>{get_lines_xml(lines[1:])}</p>
-</d:entry>"""
-                return ret
+                index_xml = ""
+                index_xml += get_index_xml(katakana, title)
+                if "・" in katakana:
+                    index_xml += get_index_xml(katakana.replace("・", ""), title)
+                # TODO add hiragana to index
+                return get_entry_xml(title, file_id, index_xml, transcription, get_lines_xml(lines[1:]), domain)
             else:
                 # ボヘミア(бохэмиа)〔000-40-00〕
+                # TODO :スプリント, スプリント・レース(сўпуринто, сўпуринто-рэ:су)〔003-01-61〕
                 header = re.search('^(.+?)\((.+?)\)〔(\d{3}-\d{2}-\d{2})〕$', lines[0])
                 if header:
                     (katakana, transcription) = header.groups()[0:-1]
                     title = f"{katakana}"
-                    hiragana = katakana  # TODO
-                    ret = f"""<d:entry id="{file_id}" d:title="{title}">
-    {get_index_xml(hiragana, title)}
-    <h1>{title} <small>{transcription}</small></h1>
-    <p>{get_lines_xml(lines[1:])}</p>
-    </d:entry>"""
-                    return ret
+                    index_xml = ""
+                    index_xml += get_index_xml(katakana, title)
+                    if "・" in katakana:
+                        index_xml += get_index_xml(katakana.replace("・", ""), title)
+                    # TODO add hiragana to index
+                    return get_entry_xml(title, file_id, index_xml, transcription, get_lines_xml(lines[1:]))
                 else:
                     print("regex mismatch")
                     print(lines)
@@ -115,11 +121,11 @@ def get_entry_xml(path):
         return None
 
 
-def iterate_files(path):
+def iterate_files(path) -> None:
     with os.scandir(path) as it:
         for entry in it:
             if entry.name.endswith(".txt") and entry.is_file():
-                xml = get_entry_xml(entry.path)
+                xml = get_entry_xml_from(entry.path)
                 if xml:
                     print(xml)
 
